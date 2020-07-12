2738e223170b8ffd44724b7943d2501bd221a81f
aqulu <dev@aqu.lu>
2020-07-12 11:02:50 +0900

    feat (EddystoneScanner): create bleManager scan adapter

---
+++ lib/infrastructure/eddystone_scanner.dart
@@ -0,0 +1,16 @@
+import 'package:eddystone_beacon_scanner/domain/eddystone_uid.dart';
+import 'package:eddystone_beacon_scanner/infrastructure/ble_manager_safe_scan.dart';
+import 'package:eddystone_beacon_scanner/infrastructure/eddystone_uid_parser.dart';
+import 'package:flutter_ble_lib/flutter_ble_lib.dart';
+
+class EddystoneScanner {
+  final BleManager _bleManager;
+
+  const EddystoneScanner(this._bleManager);
+
+  /// start BLE scan and transforms scanResults into [EddystoneUid]s
+  Stream<EddystoneUid> scan() => _bleManager
+      .scan()
+      .where((scanResult) => scanResult.advertisementData != null)
+      .map((scanResult) => scanResult.advertisementData.toEddystoneUid());
+}
